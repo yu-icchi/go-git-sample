0abd762024469314d1a0b6c95f559b108ca0b0c0
yuta <yu.ichiko@gmail.com>
2017-06-26 00:57:54 +0900

    Add sample

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ func main() {
 	println("git clone")
 
 	git.PlainClone("/tmp/foo", false, &git.CloneOptions{
-		URL: "https://github.com/yu-ichiko/go-psd.git",
+		URL:      "https://github.com/yu-ichiko/go-psd.git",
 		Progress: os.Stdout,
 	})
 }
